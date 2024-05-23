610907e44ed5ec403d1f0f286eb0af34b7b1e010
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-05-23 18:53:29 +0530

    removed unused code and added og image

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,13 +1,10 @@
-import Footer from "@/components/footer";
-import { Heading } from "@/components/heading";
 import Image from "next/image";
-import Task from "./_component/task";
 
 export default async function Home() {
   return (
     <>
       <main className="relative pt-32  flex justify-start items-center flex-col overflow-hidden mx-auto sm:px-10 px-5  bg-gradient-to-b from-zinc-950 to-zinc-950 via-slate-900">
-        <div className=" max-w-3xl text-center">
+        <div className=" max-w-3xl text-center mb-10">
           <h1 className="max-md:text-4xl md:text-6xl lg:text-7xl text-neutral-200">
             Efficiently manage your tasks with{" "}
             <span className="tracking-wide bg-gradient-to-r bg-clip-text text-transparent from-indigo-500  to-indigo-300 via-indigo-600 hover:cursor-pointer animate-text">
@@ -31,9 +28,14 @@ export default async function Home() {
             </a>
           </h2>
         </div>
-        <div className=" bg-white my-10 ">
-        <img width="auto" height="auto" src="/home.png" alt="database diagram" className="hidden lg:block" />
-          {/* <Task/> */}
+        <div className="max-w-7xl my-10 ">
+          <img
+            width="auto"
+            height="auto"
+            src="/home.png"
+            alt="database diagram"
+            className="hidden lg:block"
+          />
           <Image
             src={
               "https://img.playbook.com/gNLnzGzI3tAOqAgapBR4RrpoUSEGCvElo3CgE36VKg4/Z3M6Ly9wbGF5Ym9v/ay1hc3NldHMtcHVi/bGljLzMxZmU5OTRh/LTQ5NTYtNGRhYS1h/NTFjLThmYjUzYTRh/ZmI5OQ"
@@ -41,7 +43,7 @@ export default async function Home() {
             width={500}
             height={500}
             alt="tasks"
-            className="overflow-none object-cover lg:hidden"
+            className="overflow-none object-center lg:hidden"
           />
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-2  my-10">
@@ -68,8 +70,8 @@ export default async function Home() {
               <h1 className="text-xl">
                 Once you login you will have the option to create a
                 <span className="text-indigo-300">&nbsp;workspace</span>. Once
-                the workspace is created. You can add members, create tasks
-                and boards.
+                the workspace is created. You can add members, create tasks and
+                boards.
               </h1>
             </div>
           </div>
@@ -97,9 +99,6 @@ export default async function Home() {
               <h1 className="text-xl">
                 Made using Next.js, EdgeDB for both database and auth,
                 uploadthing for image upload, tailwind css and shadcn/ui
-                {/* <span className="text-indigo-300">
-                  track of your tasks
-                </span>{" "} */}
               </h1>
             </div>
           </div>
@@ -111,11 +110,8 @@ export default async function Home() {
             </div>
             <div className="max-w-sm flex justify-center text-justify text-muted-foreground items-center mt-3">
               <h1 className="text-xl">
-                Task is a fundamental units of work, serving a specific
-                purpose towards achieving a larger goal.
-                {/* <span className="text-indigo-300">
-                  track of your tasks
-                </span>{" "} */}
+                Task is a fundamental units of work, serving a specific purpose
+                towards achieving a larger goal.
               </h1>
             </div>
           </div>
@@ -129,7 +125,14 @@ export default async function Home() {
           </p>
         </div>
         <div className="max-w-4xl my-10">
-          <img width="auto" height="auto" src="/diagram.svg" alt="database diagram" />
+          <a href="https://productivus.vercel.app/diagram.svg" target="_blank" rel="noopener noreferrer">
+            <img
+              width="auto"
+              height="auto"
+              src="/diagram.svg"
+              alt="database diagram"
+            />
+          </a>
         </div>
       </main>
     </>
