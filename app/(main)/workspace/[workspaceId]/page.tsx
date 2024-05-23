54eadcc89d2128da4b9f9c78af111de588597c03
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-05-23 14:08:14 +0530

    implemented route redirect on /workspace/id route

--- app/(main)/workspace/[workspaceId]/page.tsx
+++ app/(main)/workspace/[workspaceId]/page.tsx
@@ -1,15 +1,8 @@
 import { Card, CardHeader } from "@/components/ui/card";
+import { redirect } from "next/navigation";
 
 const Page = async ({ params }: { params: { workspaceId: string } }) => {
-  return (
-    <>
-      <div className="pt-[50px]  flex justify-center items-center text-center">
-        <Card className=" ml-5">
-          <CardHeader>{params.workspaceId}</CardHeader>
-        </Card>
-      </div>
-    </>
-  );
+  redirect(`${params.workspaceId}/tasks`);
 };
 
 export default Page;
